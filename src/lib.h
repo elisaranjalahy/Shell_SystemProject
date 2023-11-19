267519799d82dbf7a839665b1daa755ae9dc4423
Elisa Ranjalahy <elisa.ranjalahy@etu.u-paris.fr>
2023-11-19 21:33:38 +0100

    correction commentaire

--- src/lib.h
+++ src/lib.h
@@ -12,13 +12,12 @@
 * Affiche le chemin absolue du répertoire de travail courant
  * @param cur the current path
  **/
-
  int pwd(char* cur);
 
  /**
  * Change le répertoire courant si directory est valide.
  * @param cur le chemin courant
- * @param query la recherche de l'utilisateur
+ * @param directory la recherche de l'utilisateur
  * @return 1 en cas d'erreur, sinon 0
  */
 int my_cd(char* cur, char** directory);
