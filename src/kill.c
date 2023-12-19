1ce7b8b09963d98e1f24ea1727957655a15c8ad5
SALLIN corentin <corentin.sallin@etu.u-paris.fr>
2023-12-19 12:17:49 +0100

    dependencies

---
+++ src/kill.c
@@ -0,0 +1,36 @@
+#include "utils.h"
+
+int my_kill(int argc, char** argv, job_list* jobs){
+    int sig; char* ptet_pid; pid_t pid;
+    if (argc < 1 || argc > 3) {
+        fprintf(stderr, "kill: invalid number of arguments\n");
+        return 1;
+    } if (argc == 2) {
+        sig = SIGTERM;
+        ptet_pid = argv[1];
+    } else {
+        sig = atoi(argv[1]+1);
+        ptet_pid = argv[2];
+    }
+
+    if (ptet_pid[0] == '%'){
+        int v = atoi(ptet_pid+1);
+        job_node* acc = jobs->head;
+        while (--v && acc){
+            // On part du principe que c'est positif, sinon malaise
+            acc = acc->next;
+        } if (v) {
+            fprintf(stderr, "requested job does not exist\n");
+            return 1;
+        } pid = acc->pid;
+    } else {
+        pid = atoi(ptet_pid);
+    }
+
+    if (kill(pid, sig) < 0){
+        perror("kill");
+        return 1;
+    }
+    //maj_etat_jobs(jobs);
+    return 0;
+}
